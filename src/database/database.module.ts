0ce1f24697fe60d56d71934f68a62088739a6a59
Ahmet Melih Kızmaz <melihkizmaz@icloud.com>
2022-07-02 23:18:27 +0300

    Create user repository

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user/user.entity/user.entity';
+import { UserRepository } from './user/user.entity/user.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [],
-  exports: [],
+  providers: [UserRepository],
+  exports: [UserRepository],
 })
 export class DatabaseModule {}
